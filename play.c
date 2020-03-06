46070f5d97a4251ea201a44f4084b9732af0c96e
R0kkxSynetique <60880661+R0kkxSynetique@users.noreply.github.com>
2020-03-06 10:59:25 +0100

    Change the board and accept a char as coordinates
    Embelissement de la grille et ajout de la possibilité d'entrer un coordonées (A-J ou 1 - 10)

--- play.c
+++ play.c
@@ -16,26 +16,34 @@
  */
 void jeu() {
 
+    char x;
     int board[MAX_LINE][MAX_ROW];
-    int x, y, bateaux = 1;
+    int y, bateaux = 1, lineNumber, convertedX;
 
     //definit la valeur par defaut du tableau a 0
     memset(board, 0, sizeof(board));
 
     do {
+        lineNumber = 1;
         //affiche la premiere ligne de la grille
         printf("\n");
-        for (int i = 0; i < 4 * MAX_ROW; i++) {
+        printf("     A   B   C   D   E   F   G   H   I   J\n   ");
+        for (int i = 0; i < 4 * MAX_ROW + 1; i++) {
             printf("%c", 205);
         }
-        //affiche le dernier caractere de la premiere ligne
-        printf("%c\n", 205);
+        printf("\n");
 
 
         //affiche les bordure par rapport a la taille de la grille
         for (int i = 0; i < MAX_ROW; i++) {
-
-            //affiche les bordures verticale inter-ligne
+            if (lineNumber < 10) {
+                printf("%d  ", lineNumber);
+            }
+            if (lineNumber == 10) {
+                printf("%d ", lineNumber);
+            }
+            lineNumber += 1;
+            //affiche les bordures vertical inter-ligne
             for (int j = 0; j < MAX_LINE; j++) {
                 printf("%c %d ", 186, board[i][j]);
             }
@@ -44,27 +52,32 @@ void jeu() {
             printf("%c\n", 186);
 
             //affiche les bordure horizontale
-            for (int k = 0; k < 4 * MAX_LINE; k++) {
+            printf("   ");
+            for (int k = 0; k < 4 * MAX_LINE + 1; k++) {
                 printf("%c", 205);
             }
-            //affiche la derniere bordure horizontal
-            printf("%c\n", 205);
+            printf("\n");
         }
 
-        //recupere la valeur x
+        //recupere la valeur x en char et la converti en int
         do {
-            printf("\nx : ");
-            scanf("%d", &x);
-        } while (x <= 0 || x > MAX_ROW);
+            printf("\n(A-J) x : ");
+            fflush(stdin);
+            scanf("%c", &x);
+            x -= 65;
+        } while (x < 0 || x > MAX_ROW);
+
+        //recupere la valeur int de x
+        convertedX = (int) (x);
 
         //recupere la valeur y
         do {
-            printf("\ny : ");
+            printf("\n(1-10) y : ");
             scanf("%d", &y);
         } while (y <= 0 || y > MAX_LINE);
 
-        //modifie la valeur a la coordonee x:y
-        x -= 1;
+        //modifie la valeur pour correspondre a la plage 0:0 9:9
+        convertedX -= 1;
         y -= 1;
         board[y][x] = 1;
     } while (bateaux > 0);
